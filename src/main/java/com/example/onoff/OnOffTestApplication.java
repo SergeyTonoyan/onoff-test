fe1fd420a8c6dfff5e46c4cb92ac2604a3213f3d
Sergei Tonoian <sergei.tonoian@wemakesoftware.eu>
2025-04-20 10:24:06 +0300

    - empty spring boot app created

---
+++ src/main/java/com/example/onoff/OnOffTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.onoff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnOffTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnOffTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/onoff/OnOffTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.onoff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnOffTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
