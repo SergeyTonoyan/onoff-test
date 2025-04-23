7eade64dc73b35cb8e5e9899d0ac0dedb13dfd62
Sergei Tonoian <sergei.tonoian@wemakesoftware.eu>
2025-04-23 22:36:54 +0300

    added: - readme - minor fixes and improvements

--- src/main/java/com/example/onoff/config/SecurityConfig.java
+++ src/main/java/com/example/onoff/config/SecurityConfig.java
@@ -1,7 +1,6 @@
 package com.example.onoff.config;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
